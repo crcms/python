4b531eb0bfe3051218dd6b3c65ddeec7f2dcd0fe
crcms <crcms@crcms.cn>
2018-05-11 08:59:19 +0800

    test

---
+++ src/list/list2.py
@@ -0,0 +1,10 @@
+'''
+
+'''
+
+numList = [0,1,2,3,4,5,6,7,8]
+
+numList.append(9)
+
+num = numList.pop()
+print(num,numList)
---
+++ src/string/raw.py
@@ -0,0 +1,21 @@
+#
+'''
+原始字符串
+'''
+
+#单引号
+string1 = r" raw st\ri:$\ng  ' ''' adfadsafsa s "
+print(string1)
+
+print('==========================')
+
+#双引号
+string2 = r'fdsafdsa" ''fdsafsad \ $fdas '
+print(string2)
+
+
+print('==========================')
+
+#三引号
+string3 = ''' \ xx' x  "xxxx"\ '''
+print(string3)
